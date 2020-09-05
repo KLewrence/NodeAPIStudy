8745fb8b8e157aae0e89a45adbfb23c8f3259c11
Brenda <asiathizumi@hotmail.com>
2020-09-05 14:46:56 -0500

    add log middleware

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+// MIDDLEWARES////////////////////////////////
 const bodyParser = require ('body-parser')
 app.use(bodyParser.json());
+
+app.use ((req,res,next) => {
+    console.log(`${req.method}: ${req.url}`);
+    next ();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,6 +18,7 @@ app.get('/', (req, res) => {
 app.get('/lolas', (req, res) => {
     res.send('Hello Loli!')
   })
+
 
 // ROUTERS////////////////////////////////
 const todoRouter  = require ("./routes/todo");
