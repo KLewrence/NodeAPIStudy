1b8ffdcc2569000559e699e315dcccf0c2287371
Brenda <asiathizumi@hotmail.com>
2020-09-05 14:26:59 -0500

    create todo router

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require ('body-parser')
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/lolas', (req, res) => {
+    res.send('Hello Loli!')
+  })
+
+// ROUTERS////////////////////////////////
+const todoRouter  = require ("./routes/todo");
+app.use ("/api", todoRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
