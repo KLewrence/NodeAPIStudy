8c61435d11d1423698c7435befc5764ffd08a294
Brenda <asiathizumi@hotmail.com>
2020-10-24 16:14:51 -0500

    parametrize

--- cucumber/steps/todos.js
+++ cucumber/steps/todos.js
@@ -1,9 +1,14 @@
 const {Given, Then} =  require ('cucumber');
 const axios = require('axios');
 const assert = require('assert');
+const yargs = require ('yargs/yargs')(process.argv).argv;
+//const API_URL = process.env.API_URL || 'http://localhost:3001'//'http://3.22.186.4:3000'
+console.log(process.argv);
+console.log(yargs);
+let API_URL = yargs.API_URL || 'http://localhost:3001'
 
 Given ('get all ep is requested', async function(){
-    const url = 'http://localhost:3000/api/todo'
+    const url = `${API_URL}/api/todo`
     try {
         const response = await axios.get(url);
          const data = response.data;
